582d581617b3d9ed6486a1efc407e8972defaf6f
shivv2430 <2430.shivanipal@gmail.com>
2026-04-29 20:07:57 +0530

    Control statement

---
+++ Control-statement/break.py
@@ -0,0 +1,8 @@
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+# Output- 1
+#          2
+#          3
+#          4
---
+++ Control-statement/pass.py
@@ -0,0 +1,10 @@
+for letter in "Python":
+  if letter == "h":
+    pass  # Nothing happens here
+    print(letter)
+# Output- P
+#         y
+#         t
+#         h
+#         o
+#         n
