95f6e6f8f6f102829b050c24f1904aa4dfe94546
shivv2430 <2430.shivanipal@gmail.com>
2026-04-29 20:04:52 +0530

    Loops

---
+++ Loops/for.py
@@ -0,0 +1,15 @@
+# Used to iterate over a sequence (like list, tuple, string, etc.)
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+  print(fruit)
+# Output- apple
+#         banana
+#         cherry
+
+for i in range(1, 6):
+      print(i)
+# Output- 1
+#         2
+#         3
+#         4 
+#         5           
---
+++ Loops/while.py
@@ -0,0 +1,7 @@
+count = 1
+while count <= 5:
+  print("Count:", count)
+  count += 1
+# Output-  Count: 1
+#          Count: 2
+#          Count: 3
