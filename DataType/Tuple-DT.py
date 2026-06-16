e4469d5f7a4b1ed1999c81d4172e28960fc2f2de
shivv2430 <2430.shivanipal@gmail.com>
2026-06-16 19:39:11 +0530

    Tuple

--- DataType/Tuple-DT.py
+++ DataType/Tuple-DT.py
@@ -3,3 +3,10 @@ tup1 = ()
 
 tup2 = ('Geeks', 'For')
 print("\nTuple with the use of String: ", tup2)
+
+tup1 = (1, 2, 3, 4, 5)
+
+# access tuple items
+print(tup1[0])
+print(tup1[-1])
+print(tup1[-3])
