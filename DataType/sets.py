735deb6971bb2e7e19d85283039bd27ab81a5725
shivv2430 <2430.shivanipal@gmail.com>
2026-06-16 19:49:24 +0530

    set

--- DataType/sets.py
+++ DataType/sets.py
@@ -6,3 +6,13 @@ print("Set with the use of String: ", s1)
 
 s2 = set(["Geeks", "For", "Geeks"])
 print("Set with the use of List: ", s2)
+
+set1 = set(["Geeks", "For", "Geeks"]) #Duplicates are removed automatically
+print(set1) 
+
+# loop through set
+for i in set1:
+   print(i, end=" ") #prints elements one by one
+  
+# check if item exist in set   
+print("Geeks" in set1)
