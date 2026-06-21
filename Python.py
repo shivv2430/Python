de5d71a0c63e8bf7a5f2bcd3fcc55f19fa6f340d
shivv2430 <2430.shivanipal@gmail.com>
2026-06-21 21:48:15 +0530

    add 2 number

--- Python.py
+++ Python.py
@@ -4,3 +4,10 @@ print("Hello, World!")
 #Take Input from User
 name = input("Enter your name: ")
 print("Hello", name)
+
+#Add Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+print("Sum =", sum)
