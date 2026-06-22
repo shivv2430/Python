f22a1d0a40905f9ee033c68601fd6b5d6636630a
shivv2430 <2430.shivanipal@gmail.com>
2026-06-22 23:36:05 +0530

    sum of first N numbers

--- Python.py
+++ Python.py
@@ -50,3 +50,12 @@ else:
 #Print Numbers 1 to 10
 for i in range(1, 11):
     print(i)
+
+#Find Sum of First N Numbers
+n = int(input("Enter n: "))
+
+total = 0
+for i in range(1, n + 1):
+    total += i
+
+print("Sum =", total)
