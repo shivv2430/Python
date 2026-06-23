f534c1711459c3e0bf94cd6a64afa78ef0828b8f
shivv2430 <2430.shivanipal@gmail.com>
2026-06-23 20:20:37 +0530

    simple calculator

--- Python.py
+++ Python.py
@@ -111,3 +111,12 @@ def greet(name):
     print("Hello", name)
 
 greet("Shivani")
+
+#Simple Calculator
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
