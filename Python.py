98ebfc0706ed06b68a81d011b373f67ee4ff49ee
shivv2430 <2430.shivanipal@gmail.com>
2026-07-04 23:56:40 +0530

    Check Whether a Year is a Leap Year

--- Python.py
+++ Python.py
@@ -139,3 +139,12 @@ b = temp
 print("After swapping:")
 print("a =", a)
 print("b =", b)
+
+
+#Check Whether a Year is a Leap Year
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
