4e87d4f8265e9b133ea80a1652bd8f8d1877ddbf
shivv2430 <2430.shivanipal@gmail.com>
2026-07-06 23:29:16 +0530

    Count Words in a Sentence

--- Python.py
+++ Python.py
@@ -192,3 +192,10 @@ rows = int(input("Enter number of rows: "))
 
 for i in range(1, rows + 1):
     print(" " * (rows - i) + "*" * (2 * i - 1))
+
+#Count Words in a Sentence
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+print("Total words =", len(words))
