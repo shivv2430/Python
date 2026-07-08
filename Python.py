24de82105119f5d21923c1c9076e90d1a380cb90
shivv2430 <2430.shivanipal@gmail.com>
2026-07-08 23:40:32 +0530

    Count the Number of Digits

--- Python.py
+++ Python.py
@@ -225,3 +225,14 @@ for num in numbers:
         smallest = num
 
 print("Smallest =", smallest)
+
+#Count the Number of Digits
+number = int(input("Enter a number: "))
+
+count = 0
+
+while number > 0:
+    number = number // 10
+    count = count + 1
+
+print("Total digits =", count)
